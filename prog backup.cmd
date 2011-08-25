b23c6ef8ab89da351762de28b292d81e7ee44619
KrasheninnikovMaxim <maxk008@gmail.com>
2011-08-25 05:47:37 +0000

    git-svn-id: https://5-10/svn/tstlan4@21 d66dc6fa-fed5-4241-8511-616b76b84d51

--- prog backup.cmd
+++ prog backup.cmd
@@ -6,4 +6,4 @@ rar a -rr -r -ep1 -ap%progname% -x*.obj -x*.tds -x*.~* -x*.il? -x*.bak -x*.*~^
   -x*.dcu -x~W*.tmp -x~$*.* -x*.map -x*.pch "-ag yyyymmdd hhmmss"^
   "%lib%\%progname%.rar" .
 
-pause
+if "%~1"=="" pause
