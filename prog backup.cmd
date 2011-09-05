aeb7eb836f664fe2e0cada3c23192945214c2073
KrasheninnikovMaxim <maxk008@gmail.com>
2011-09-05 08:03:16 +0000

    git-svn-id: https://5-10/svn/tstlan4@28 d66dc6fa-fed5-4241-8511-616b76b84d51

--- prog backup.cmd
+++ prog backup.cmd
@@ -5,7 +5,7 @@ set progname=tstlan4
 if "%~1"=="" (set lib=..\old) else (set lib=%~1)
 
 rar a -rr -r -ep1 -ap%progname% -x*.obj -x*.tds -x*.~* -x*.il? -x*.bak -x*.*~^
-  -x*.dcu -x~W*.tmp -x~$*.* -x*.map -x*.pch "-ag yyyymmdd hhmmss"^
+  -x*.dcu -x~W*.tmp -x~$*.* -x*.map -x*.pch -x*.#?? "-ag yyyymmdd hhmmss"^
   "%lib%\%progname%.rar" .
 
 if "%~1"=="" pause
