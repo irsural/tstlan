8c5c6b92a8bfd250b50a8e687f2d59819927b503
KrasheninnikovMaxim <maxk008@gmail.com>
2025-08-13 15:10:47 +0500

    mxstclib: Исправление отладочного кода внутри класса modbus_assembly_t для проверки класса simple_ftp_client_t c fs_t и без param_box_base_t

--- tstlan.cpp
+++ tstlan.cpp
@@ -1,5 +1,5 @@
 //---------------------------------------------------------------------------
-// tstlan rev. 111 mxsrclib rev. 1596 extern_libs rev. 37
+// tstlan rev. 112 mxsrclib rev. 1597 extern_libs rev. 37
 //---------------------------------------------------------------------------
 #include <vcl.h>
 #pragma hdrstop
