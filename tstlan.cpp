1ba1102615f0b09d7bf49ee67ea620dd823a3598
KrasheninnikovMaxim <maxk008@gmail.com>
2025-08-13 16:23:02 +0500

    mxstclib: Класс simple_ftp_client_t вынесен в отдельный модуль

--- tstlan.cpp
+++ tstlan.cpp
@@ -1,5 +1,5 @@
 //---------------------------------------------------------------------------
-// tstlan rev. 112 mxsrclib rev. 1597 extern_libs rev. 37
+// tstlan rev. 114 mxsrclib rev. 1598 extern_libs rev. 37
 //---------------------------------------------------------------------------
 #include <vcl.h>
 #pragma hdrstop
