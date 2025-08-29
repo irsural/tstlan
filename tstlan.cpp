4cf1580604323c32614641cf29359066566864e9
KrasheninnikovMaxim <maxk008@gmail.com>
2025-08-29 19:23:22 +0500

    mxsrclib: simple_ftp_client_t Реализовано чтение информации о содержимом папки в файл

--- tstlan.cpp
+++ tstlan.cpp
@@ -1,5 +1,5 @@
 //---------------------------------------------------------------------------
-// tstlan rev. 122 mxsrclib rev. 1610 extern_libs rev. 37
+// tstlan rev. 123 mxsrclib rev. 1611 extern_libs rev. 37
 //---------------------------------------------------------------------------
 #include <vcl.h>
 #pragma hdrstop
