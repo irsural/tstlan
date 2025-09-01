dd265f3aa2888e46f6f445fc719975063c26394a
KrasheninnikovMaxim <maxk008@gmail.com>
2025-09-01 15:29:09 +0500

    mxsrclib: dir_iterator_t Добавлена функция next_to_begin()

--- tstlan.cpp
+++ tstlan.cpp
@@ -1,5 +1,5 @@
 //---------------------------------------------------------------------------
-// tstlan rev. 125 mxsrclib rev. 1614 extern_libs rev. 37
+// tstlan rev. 126 mxsrclib rev. 1615 extern_libs rev. 37
 //---------------------------------------------------------------------------
 #include <vcl.h>
 #pragma hdrstop
