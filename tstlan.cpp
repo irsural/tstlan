d23a506808d9ff4c79fd4bc544063b1ec82f0625
KrasheninnikovMaxim <maxk008@gmail.com>
2025-09-05 13:02:12 +0500

    mxsrclib: simple_ftp_client_t Номер канала передается в конструкторе

--- tstlan.cpp
+++ tstlan.cpp
@@ -1,5 +1,5 @@
 //---------------------------------------------------------------------------
-// tstlan rev. 132 mxsrclib rev. 1622 extern_libs rev. 37
+// tstlan rev. 133 mxsrclib rev. 1623 extern_libs rev. 37
 //---------------------------------------------------------------------------
 #include <vcl.h>
 #pragma hdrstop
