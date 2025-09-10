f0c29dcfb11731b2ee234d40babca22621afffb3
KrasheninnikovMaxim <maxk008@gmail.com>
2025-09-10 18:39:12 +0500

    mxsrclib: simple_ftp_client_t Исправлено исправление ошибка обработки последней ошибки

--- tstlan.cpp
+++ tstlan.cpp
@@ -1,5 +1,5 @@
 //---------------------------------------------------------------------------
-// tstlan rev. 139 mxsrclib rev. 1631 extern_libs rev. 37
+// tstlan rev. 140 mxsrclib rev. 1632 extern_libs rev. 37
 //---------------------------------------------------------------------------
 #include <vcl.h>
 #pragma hdrstop
