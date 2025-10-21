449dab3e1c352d04607c167c181bc4b942c5e045
KrasheninnikovMaxim <maxk008@gmail.com>
2025-10-21 12:58:00 +0500

    mxsrclib: modbus_assembly_t (tstlan) Решение проблемы прерывания передачи файлов по Simple FTP из-за перезапуска USB HID

--- tstlan.cpp
+++ tstlan.cpp
@@ -1,5 +1,5 @@
 //---------------------------------------------------------------------------
-// tstlan rev. 146 mxsrclib rev. 1638 extern_libs rev. 37
+// tstlan rev. 147 mxsrclib rev. 1639 extern_libs rev. 37
 //---------------------------------------------------------------------------
 #include <vcl.h>
 #pragma hdrstop
